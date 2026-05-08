5a50962e699849f5e48ae42e1bc0d718f10dd635
Aditya2005-cloud <adityasahaclass11f@gmail.com>
2026-05-08 19:37:00 +0530

    8/5/20226

---
+++ day5_IndexSlicing.py
@@ -0,0 +1,15 @@
+name="Kaka"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+#or
+print("____________________________________")
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+#or
+print("____________________________________")
+for i in range(len(name)):
+    print(name[i])
