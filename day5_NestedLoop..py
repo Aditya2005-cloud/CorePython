7ddbce7d9b27a5a9efae42e43c0136f9ca74a21c
Aditya2005-cloud <adityasahaclass11f@gmail.com>
2026-05-08 20:52:03 +0530

    8/5/2026

--- day5_NestedLoop..py
+++ day5_NestedLoop..py
@@ -42,3 +42,40 @@ for i in range(5,0,-1):
     for a in range(0,i):
         print("*",end=" ")
     print()
+#Student Marks Analyzer
+# Program should:
+# Find highest marks
+# Find average
+# Count passed students (≥ 40)
+marks = [78, 45, 90, 34, 88]
+highest_marks=0
+average=0
+count=0
+for i in marks:
+    if i>highest_marks:
+        highest_marks=i
+    average+=i
+    if i>=40:
+        count+=1
+average=average/len(marks)
+print(f"the highest marks are {highest_marks}")
+print(f"the average marks are {average}")
+print(f"the number of passed students are {count}")
+#Find smallest number in list
+min_num=marks[0]
+for i in marks:
+    if i<min_num:
+        min_num=i
+print(f"the smallest number in the list is {min_num}")
+#Remove duplicates from list
+marks1=list(set(marks))#remove duplicates
+print(marks1)
+#Count odd numbers
+count1=0
+for i in marks:
+    if i%2!=0:
+        count1+=1
+print(f"the number of odd numbers are {count1}")
+#Reverse list WITHOUT [::-1]
+for i in range(len(marks)-1,-1,-1):
+    print(marks[i])
